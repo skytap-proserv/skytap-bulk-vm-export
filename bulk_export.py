a94177cf0f4236a5400357fe3d719fb35e62f5e6
W. S. Wellington <bwellington@skytap.com>
2017-01-03 11:51:42 -0800

    list existing jobs

--- bulk_export.py
+++ bulk_export.py
@@ -7,11 +7,16 @@ from skytap.Templates import Templates
 from skytap.Exports import Exports
 
 template = Templates()[936865]
+jobs= Exports()
 
 # get list of vm ids in the template
 def list_vms():
     for vm in template.vms:
         print vm.id
+
+def list_jobs():
+    for job in jobs:
+        print job.id, job.vm_name
 
 
 if __name__ == '__main__':
