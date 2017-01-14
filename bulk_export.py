3008ea908ad32376c577165b4fa9d190cb6f822a
W. S. Wellington <bwellington@skytap.com>
2017-01-14 10:15:19 -0800

    complete rewrite
    rewrite to include treading and queueing.

--- bulk_export.py
+++ bulk_export.py
@@ -1,174 +1,138 @@
-"""
-docstring here
 
-"""
+
+
 import sys
 import urllib
 from time import sleep
 import json
-import pickle
 import argparse
+import threading
+import Queue
+import logging
 
 from skytap.Templates import Templates
 from skytap.Exports import Exports
 
 download_dir = '/Users/bwellington/Downloads'
-queue_file = "queue.p"
-download_file = "downloads.p"
 
-jobs = Exports()
+logger = logging.getLogger('bulk-vm-export')
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
 
-def pickle_add(l, pickle_file):
-    c = pickle_load(pickle_file)
-    p = list(set(l).union(c))
-    pickle_dump(p, pickle_file)
+vm_queue = Queue.Queue(maxsize = 0)
+dl_queue = Queue.Queue(maxsize = 0)
 
-def pickle_remove(i, pickle_file):
-    c = pickle_load(pickle_file)
-    while i in c: c.remove(i)
-    pickle_dump(c, pickle_file)
 
-def pickle_dump(l, pickle_file):
-    try:
-        pickle.dump( l, open( pickle_file, "wb" ) )
-    except:
-        print "could not write to pickle file"
-
-def pickle_load(pickle_file):
-    try:
-        l = pickle.load(open( pickle_file, "rb" ) )
-    except:
-        l = []
-
-    return l
-
-def pickle_clear(pickle_file):
-    l = []
-    pickle_dump(l, pickle_file)
-
-def pickle_list(pickle_file):
-    l = pickle_load(pickle_file)
-    print l
-
-# get list of vm ids in the template
-def list_vms(template_id):
+def build_vm_queue(template_id):
+    '''Load all the VMs from the template into the vm_queue'''
     template = Templates()[template_id]
     for vm in template.vms:
-        print vm.id
+        vm_queue.put(vm.id)
 
-def queue_jobs(template_id):
-    template = Templates()[template_id]
-    queue = pickle_load(queue_file)
-    for vm in template.vms:
-        queue.append(vm.id)
-    pickle_add(queue, queue_file)
-    return queue
-
-
-def create_jobs():
-    queue = pickle_load(queue_file)
-    for vm in queue:
+def create_jobs(vm_queue):
+    '''pop a VM id from the vm_queue and create a Skytap export job.'''
+    while True:
+#        logger.debug(threading.current_thread().name,"exporter")
+        vm = vm_queue.get()
+        logger.info("Create export job for VM " + str(vm) +".")
         try:
-            Exports().create(vm)
+            j = Exports().create(vm)
             sleep(5)
-            while vm in queue:
-                queue.remove(vm)
-                pickle_remove(vm, queue_file)
+            job = Exports()[j]
+            logger.info("Add export Job " + str(job.id) + " to dl_queue.")
+            dl_queue.put(job) # add new export object to the dl_queue
+            vm_queue.task_done()
+
+        except Queue.Empty:
+            logger.error("The vm_queue is empty.")
+            continue
+
         except Exception as e:
             err = e.message
-            if (err['status_code'] == 422):
-                print "All import/export slots full"
-            print "Error creating export jobs"
-            pass
+            print err
+            # if (err['status_code'] == 422):
+            #     print("All import/export slots full.  Returning " + str(vm) + " to queue.")
+            # else:
+            #     print("Error creating export jobs.")
 
-def list_jobs():
+            vm_queue.put(vm)
+            sleep(60)
+            continue
+
+def build_download_queue():
+    '''add the existing list of exports to the dl_queue'''
+    logger.info("Check for existing exports and add them to the dl_queue.")
+    jobs = Exports()
     for job in jobs:
-        print job.id, job.vm_name, job.status
+        dl_queue.put(job)
 
-def download_export():
-    for job in jobs:
-        if job.status == 'complete':
-            try:
-                active = pickle_load(download_file)
-                if job.id not in active:
-                    active.append(job.id)
-                    pickle_add(active, download_file)
-                    print "Downloading Job " + str(job.id)
-                    urllib.urlretrieve(job.ftp_url, download_dir + '/' +
-                    str(job.id) + '_' +job.vm_name + '.7z')
-                    delete_job(job.id)
-                    sleep(5)
-                    active = pickle_load(download_file)
-                    while job.id in active:
-                        active.remove(job.id)
-                        pickle_remove(job.id, download_file)
-                else:
-                    pass
 
-            except:
-                print "Unable to download job " + str(job.id)
-                pass
+def download_jobs(dl_queue):
+    '''download export jobs form skytap and cleanup when finished'''
+    while True:
+#        logger.debug(threading.current_thread().name,"downloader")
+        j = dl_queue.get()
+        try:
+            if j.status == "processing":
+                logger.info("Job " + str(j.id) + " Status: " + str(j.status))
+                refresh_job = Exports()[j.id]
+                dl_queue.put(refresh_job)
+                dl_queue.task_done
 
-        elif job.status == 'processing':
-            print 'Job ' + str(job.id) + ' is still processing.'
+            elif j.status == 'complete':
+                logger.info("Job " + str(j.id) + " Status: " + str(j.status))
+                logger.info("Downloading Job " + str(j.id))
+                urllib.urlretrieve(j.ftp_url, download_dir + '/' +
+                str(j.id) + '_' +j.vm_name + '.7z')
+                delete_job(j.id)
+                dl_queue.task_done()
+            else:
+                dl_queue.put(j)
+                logger.info("Job " + str(j.id) + " Status: " + str(j.status))
 
-        elif job.status == 'error':
-            print 'Error creating export job ' + str(job.id)
+        except Queue.Empty:
+            logger.error("The dl_queue is empty.")
+            continue
 
-        else:
-            print 'An unspecified error has occured.'
+        sleep(60)
 
 def delete_job(id):
     Exports().delete(id)
 
-def delete_jobs():
-    for job in jobs:
-        Exports().delete(job.id)
 
-
-
-
+##########################
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument("-q", "--queue", help="Queue up all the VMs in the template for exporting.  Takes a template_id as an argument.")
-    group.add_argument("-e", "--export", action="store_true", default=False, help="Create export jobs from the VMs currently in the queue.")
-    group.add_argument("-d", "--download", action="store_true", default=False, help="Download \"completed\" export job from Skytap.")
-    group.add_argument("--list_exports", action="store_true", default=False, help="Display current exports.")
-    group.add_argument("--list_active_downloads", action="store_true", default=False, help="Display active downloads.")
-    group.add_argument("--list_queue", action="store_true", default=False, help="Display current queue.")
-    group.add_argument("--clear_queue", action="store_true", default=False, help="Clear all remaining items in the queue.")
-    group.add_argument("--clear_active_downloads", action="store_true", default=False, help="Clear all remaining active downloads.")
-    group.add_argument("--delete_all", action="store_true", default=False, help="This will delete all completed exports and clear the queue.")
+    parser.add_argument("-t", "--template", help="Queue up all the VMs in the template for exporting." +
+    "  Takes a template_id as an argument.")
+    parser.add_argument("-d", "--downloaders", help="Number of download threads to use." +
+    " This number is limited only by available memory.  However, Skytap accounts have a specific " +
+    " number of import/export depots.  It will serve no purpose to have more download threads" +
+    " than you have export depots. Default: 3 ")
+    result = parser.parse_args()
 
-    result =  parser.parse_args()
+    if result.downloaders > 0:
+        dl_threads = int(result.downloaders)
+    else:
+        dl_threads = 3
 
-    if result.queue:
-        queue_jobs(int(result.queue))
+    for i in range(dl_threads):
+        '''Set up the download threads.'''
+        downloader = threading.Thread(target=download_jobs, args=(dl_queue,))
+        downloader.setDaemon(False)
+        downloader.start()
 
-    if result.export:
-        create_jobs()
+    template_id = int(result.template)
+    if result.template:
+        '''Set up the vm queue thread.'''
+        build_vm_queue(template_id)
 
-    if result.download:
-        download_export()
+        exporter = threading.Thread(target=create_jobs, args=(vm_queue,))
+        exporter.setDaemon(False)
+        exporter.start()
 
-    if result.list_exports:
-        list_jobs()
-
-    if result.list_queue:
-        pickle_list(queue_file)
-
-    if result.list_active_downloads:
-        pickle_list(download_file)
-
-    if result.clear_queue:
-        pickle_clear(queue_file)
-
-    if result.clear_active_downloads:
-        pickle_clear(download_file)
-
-    if result.delete_all:
-        delete_jobs()
-        pickle_clear(queue_file)
-        pickle_clear(download_file)
+    build_download_queue()
