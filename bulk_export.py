fc5a04ef1a37fd5fafa97245391b5930bf6107e0
W. S. Wellington <bwellington@skytap.com>
2017-01-14 10:11:39 -0800

    this does the job, but there are too many opportunities for race conditions.

--- bulk_export.py
+++ bulk_export.py
@@ -14,30 +14,41 @@ from skytap.Exports import Exports
 
 download_dir = '/Users/bwellington/Downloads'
 queue_file = "queue.p"
+download_file = "downloads.p"
 
 jobs = Exports()
 
-def queue_dump(queue):
+def pickle_add(l, pickle_file):
+    c = pickle_load(pickle_file)
+    p = list(set(l).union(c))
+    pickle_dump(p, pickle_file)
+
+def pickle_remove(i, pickle_file):
+    c = pickle_load(pickle_file)
+    while i in c: c.remove(i)
+    pickle_dump(c, pickle_file)
+
+def pickle_dump(l, pickle_file):
     try:
-        pickle.dump( queue, open( queue_file, "wb" ) )
+        pickle.dump( l, open( pickle_file, "wb" ) )
     except:
         print "could not write to pickle file"
 
-def queue_load():
+def pickle_load(pickle_file):
     try:
-        queue = pickle.load( open( queue_file, "rb" ) )
+        l = pickle.load(open( pickle_file, "rb" ) )
     except:
-        queue = []
+        l = []
 
-    return queue
+    return l
 
-def clear_queue():
-    queue = []
-    queue_dump(queue)
+def pickle_clear(pickle_file):
+    l = []
+    pickle_dump(l, pickle_file)
 
-
-# def dequeue(vm):
-#     while vm in queue: queue.remove(vm)
+def pickle_list(pickle_file):
+    l = pickle_load(pickle_file)
+    print l
 
 # get list of vm ids in the template
 def list_vms(template_id):
@@ -47,51 +58,62 @@ def list_vms(template_id):
 
 def queue_jobs(template_id):
     template = Templates()[template_id]
-    queue = queue_load()
+    queue = pickle_load(queue_file)
     for vm in template.vms:
         queue.append(vm.id)
-    queue_dump(queue)
+    pickle_add(queue, queue_file)
     return queue
 
 
 def create_jobs():
-    queue = queue_load()
+    queue = pickle_load(queue_file)
     for vm in queue:
         try:
             Exports().create(vm)
             sleep(5)
-            while vm in queue: queue.remove(vm)
+            while vm in queue:
+                queue.remove(vm)
+                pickle_remove(vm, queue_file)
         except Exception as e:
             err = e.message
-            print err
             if (err['status_code'] == 422):
-                print "All slots full"
-            print ("error creating export jobs")
+                print "All import/export slots full"
+            print "Error creating export jobs"
             pass
 
-        finally:
-            queue_dump(queue)
-
-# def list_jobs():
-#     for job in jobs:
-#         print job.id, job.vm_name, job.status
+def list_jobs():
+    for job in jobs:
+        print job.id, job.vm_name, job.status
 
 def download_export():
     for job in jobs:
         if job.status == 'complete':
             try:
-                urllib.urlretrieve(job.ftp_url, download_dir + '/' +
-                str(job.id) + '_' +job.vm_name + '.7z')
-                delete_job(job.id)
+                active = pickle_load(download_file)
+                if job.id not in active:
+                    active.append(job.id)
+                    pickle_add(active, download_file)
+                    print "Downloading Job " + str(job.id)
+                    urllib.urlretrieve(job.ftp_url, download_dir + '/' +
+                    str(job.id) + '_' +job.vm_name + '.7z')
+                    delete_job(job.id)
+                    sleep(5)
+                    active = pickle_load(download_file)
+                    while job.id in active:
+                        active.remove(job.id)
+                        pickle_remove(job.id, download_file)
+                else:
+                    pass
 
-            except Error:
-                print("Something went wrong with the download")
+            except:
+                print "Unable to download job " + str(job.id)
+                pass
 
-        elif joh.status == 'processing':
-            print 'Job ' + job.id + 'is still processing.'
+        elif job.status == 'processing':
+            print 'Job ' + str(job.id) + ' is still processing.'
 
         elif job.status == 'error':
-            print 'Error creating export job' + job.id
+            print 'Error creating export job ' + str(job.id)
 
         else:
             print 'An unspecified error has occured.'
@@ -113,7 +135,11 @@ if __name__ == '__main__':
     group.add_argument("-q", "--queue", help="Queue up all the VMs in the template for exporting.  Takes a template_id as an argument.")
     group.add_argument("-e", "--export", action="store_true", default=False, help="Create export jobs from the VMs currently in the queue.")
     group.add_argument("-d", "--download", action="store_true", default=False, help="Download \"completed\" export job from Skytap.")
+    group.add_argument("--list_exports", action="store_true", default=False, help="Display current exports.")
+    group.add_argument("--list_active_downloads", action="store_true", default=False, help="Display active downloads.")
+    group.add_argument("--list_queue", action="store_true", default=False, help="Display current queue.")
     group.add_argument("--clear_queue", action="store_true", default=False, help="Clear all remaining items in the queue.")
+    group.add_argument("--clear_active_downloads", action="store_true", default=False, help="Clear all remaining active downloads.")
     group.add_argument("--delete_all", action="store_true", default=False, help="This will delete all completed exports and clear the queue.")
 
     result =  parser.parse_args()
@@ -127,9 +153,22 @@ if __name__ == '__main__':
     if result.download:
         download_export()
 
+    if result.list_exports:
+        list_jobs()
+
+    if result.list_queue:
+        pickle_list(queue_file)
+
+    if result.list_active_downloads:
+        pickle_list(download_file)
+
     if result.clear_queue:
-        clear_queue()
+        pickle_clear(queue_file)
+
+    if result.clear_active_downloads:
+        pickle_clear(download_file)
 
     if result.delete_all:
         delete_jobs()
-        clear_queue()
+        pickle_clear(queue_file)
+        pickle_clear(download_file)
