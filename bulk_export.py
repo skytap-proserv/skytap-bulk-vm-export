c8e57b3c29cc916280f47e07979192c0422a4e2d
W. S. Wellington <bwellington@skytap.com>
2017-01-16 12:32:51 -0800

    cleaning up the queueing

--- bulk_export.py
+++ bulk_export.py
@@ -22,34 +22,37 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.INFO)
 
-vm_queue = Queue.Queue(maxsize = 0)
-dl_queue = Queue.Queue(maxsize = 0)
+export_queue = Queue.Queue(maxsize = 0)
 
 
-def build_vm_queue(template_id):
-    '''Load all the VMs from the template into the vm_queue'''
+def build_export_queue(template_id):
+    '''Load all the VMs from the template into the export_queue'''
     template = Templates()[template_id]
     for vm in template.vms:
-        vm_queue.put(vm.id)
+        export_queue.put(vm.id)
 
-def create_jobs(vm_queue):
-    '''pop a VM id from the vm_queue and create a Skytap export job.'''
+def create_jobs(export_queue):
+    '''pop a VM id from the export_queue and create a Skytap export job.'''
+
     while True:
 #        logger.debug(threading.current_thread().name,"exporter")
-        vm = vm_queue.get()
-        logger.info("Create export job for VM " + str(vm) +".")
+
         try:
             vm = export_queue.get(False)
+            logger.info("Create export job for VM " + str(vm) +".")
             j = Exports().create(vm)
             sleep(5)
-            job = Exports()[j]
-            logger.info("Add export Job " + str(job.id) + " to dl_queue.")
-            dl_queue.put(job) # add new export object to the dl_queue
-            vm_queue.task_done()
+
+            #create a downloader thread for this job
+            downloader = threading.Thread(target=download_job, args=(j,))
+            downloader.setDaemon(False)
+            downloader.start()
+
+            export_queue.task_done()
 
         except Queue.Empty:
-            logger.error("The vm_queue is empty.")
-            continue
+            logger.error("The export_queue is empty.")
+            return
 
         except Exception as e:
             err = e.message
@@ -66,49 +69,41 @@ def create_jobs(vm_queue):
                     print err
                     export_queue.task_done()
 
-            vm_queue.put(vm)
+            continue
+
+def download_job(j):
+#    '''download export jobs form skytap and cleanup when finished'''
+    logger.debug("Start Download Thread: " + threading.current_thread().name,str(j) + " downloader")
+    while True:
+        job = Exports()[j]
+        if job.status == "processing":
+            logger.info("Job " + str(job.id) + " Status: " + str(job.status) )
+            logger.info("Job " + str(job.id) + " Refreshing.")
+            job = Exports()[job.id]
             sleep(60)
             continue
 
-def build_download_queue():
-    '''add the existing list of exports to the dl_queue'''
-    logger.info("Check for existing exports and add them to the dl_queue.")
-    jobs = Exports()
-    for job in jobs:
-        dl_queue.put(job)
+        elif job.status == 'complete':
+            try:
+                logger.info("Job " + str(job.id) + " Status: " + str(job.status))
+                logger.info("Downloading Job " + str(job.id))
+                urllib.urlretrieve(job.ftp_url, download_dir + '/' +
+                str(job.id) + '_' +job.vm_name + '.7z')
+                delete_job(job.id)
+                logger.info("Job " + str(job.id) + " downloaded successfully!")
+                return
 
+            except:
+                logger.error("Download failed")
+                return
 
-def download_jobs(dl_queue):
-    '''download export jobs form skytap and cleanup when finished'''
-    while True:
-#        logger.debug(threading.current_thread().name,"downloader")
-        j = dl_queue.get()
-        try:
-            if j.status == "processing":
-                logger.info("Job " + str(j.id) + " Status: " + str(j.status))
-                refresh_job = Exports()[j.id]
-                dl_queue.put(refresh_job)
-                dl_queue.task_done
-
-            elif j.status == 'complete':
-                logger.info("Job " + str(j.id) + " Status: " + str(j.status))
-                logger.info("Downloading Job " + str(j.id))
-                urllib.urlretrieve(j.ftp_url, download_dir + '/' +
-                str(j.id) + '_' +j.vm_name + '.7z')
-                delete_job(j.id)
-                dl_queue.task_done()
-            else:
-                dl_queue.put(j)
-                logger.info("Job " + str(j.id) + " Status: " + str(j.status))
-
-        except Queue.Empty:
-            logger.error("The dl_queue is empty.")
-            continue
-
-        sleep(60)
+        else:
+            logger.error("Something went wrong")
+            return
 
 def delete_job(id):
     Exports().delete(id)
+
 
 
 ##########################
@@ -117,30 +112,19 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--template", help="Queue up all the VMs in the template for exporting." +
     "  Takes a template_id as an argument.")
-    parser.add_argument("-d", "--downloaders", help="Number of download threads to use." +
-    " This number is limited only by available memory.  However, Skytap accounts have a specific " +
-    " number of import/export depots.  It will serve no purpose to have more download threads" +
-    " than you have export depots. Default: 3 ")
     result = parser.parse_args()
 
-    if result.downloaders > 0:
-        dl_threads = int(result.downloaders)
-    else:
-        dl_threads = 3
-
-    for i in range(dl_threads):
-        '''Set up the download threads.'''
-        downloader = threading.Thread(target=download_jobs, args=(dl_queue,))
-        downloader.setDaemon(False)
-        downloader.start()
-
     template_id = int(result.template)
+
+
+
+
     if result.template:
         '''Set up the vm queue thread.'''
-        build_vm_queue(template_id)
+        build_export_queue(template_id)
+        create_jobs(export_queue)
 
-        exporter = threading.Thread(target=create_jobs, args=(vm_queue,))
-        exporter.setDaemon(False)
-        exporter.start()
 
-    build_download_queue()
+    #     exporter = threading.Thread(target=create_jobs, args=(export_queue,))
+    #     exporter.setDaemon(False)
+    #     exporter.start()
